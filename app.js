b67ae6df8438c6fb2a570b0fae11625655feb6be
Abhijeet-kadance <Abhijeet.kadance.96@gmail.com>
2022-04-17 23:57:41 +0530

    DockerFile creation

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express")
+
+const app = express()
+
+const port = process.env.PORT || 3000;
+
+
+app.get("/",(req,res)=>{
+    res.send("<h2>Hi There</h2>")
+})
+
+app.listen(port,()=>{
+    console.log(`listining at port ${port}`)
+})
+
